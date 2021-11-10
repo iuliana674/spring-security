0a4f7d321cdac54bceefdee7652b07ca37e9fbd6
unknown <jviuliana@gmail.com>
2021-11-10 12:03:13 +0200

    page

---
+++ src/main/java/com/example/iuliana/model/Role.java
@@ -0,0 +1,23 @@
+package com.example.iuliana.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+@Setter
+@Getter
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
