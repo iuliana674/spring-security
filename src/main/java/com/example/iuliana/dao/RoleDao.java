4f03e360c32741a037d0c8c45a7e70a3ba50705e
unknown <jviuliana@gmail.com>
2021-11-13 18:54:14 +0200

    updated

---
+++ src/main/java/com/example/iuliana/dao/RoleDao.java
@@ -0,0 +1,9 @@
+package com.example.iuliana.dao;
+
+import com.example.iuliana.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleDao extends JpaRepository<Role, Long> {
+}
